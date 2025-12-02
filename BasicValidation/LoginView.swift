e7dac8df9e3d8b2438649f2d63d8d2e0993e91fa
Kovvuri Supraj <54736412+kovvurisupraj@users.noreply.github.com>
2025-12-02 16:32:30 -0600

    Added Error messages

--- BasicValidation/LoginView.swift
+++ BasicValidation/LoginView.swift
@@ -7,22 +7,59 @@
 
 import SwiftUI
 
+struct LoginFormError{
+    var email: String = ""
+    var password: String = ""
+}
+
 struct LoginView: View {
     
     @State private var email: String = ""
     @State private var password: String = ""
+    @State private var loginformerror = LoginFormError()
+    
+    private func clearForm(){
+        loginformerror = LoginFormError()
+    }
     
     var isFormValid: Bool{
-        return !email.isEmpty && !password.isEmpty && email.isValidEmail
+        
+        clearForm()
+        
+        if email.isEmpty{
+            loginformerror.email = "Email is required"
+        } else if !email.isValidEmail{
+            loginformerror.email = "Invalid Email Format"
+        }
+        
+        if password.isEmpty{
+            loginformerror.password = "Password is required"
+        }
+        
+        return loginformerror.email.isEmpty && loginformerror.password.isEmpty
     }
+    
+    /*var isFormValid: Bool{
+        return !email.isEmpty && !password.isEmpty && email.isValidEmail
+    }*/
     
     var body: some View {
         Form{
             TextField("Email", text: $email)
+            if !loginformerror.email.isEmpty {
+                Text(loginformerror.email)
+                    .font(.caption)
+            }
             SecureField("Password", text: $password)
+            if !loginformerror.password.isEmpty{
+                Text(loginformerror.password)
+                    .font(.caption)
+            }
             Button("Login"){
-                
-            }.disabled(!isFormValid)
+                if isFormValid{
+                    
+                }
+            }
         }
     }
 }
