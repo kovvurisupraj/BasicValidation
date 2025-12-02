ae1b4b99ca4285900087191a3fe4fe32d10d3d35
Kovvuri Supraj <54736412+kovvurisupraj@users.noreply.github.com>
2025-12-02 15:34:13 -0600

    Initial Commit

---
+++ BasicValidation/BasicValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicValidationApp.swift
+//  BasicValidation
+//
+//  Created by Supraj Kovvuri on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct BasicValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
